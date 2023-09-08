9ebd5dae172205c81e87185dc1a9b3508fba14cd
EC2 Default User <ec2-user@ip-172-31-1-155.ap-northeast-1.compute.internal>
2023-09-08 01:35:58 +0000

    [Finish]8b

--- app/models/book.rb
+++ app/models/book.rb
@@ -21,5 +21,15 @@ class Book < ApplicationRecord
       Book.where('name LIKE ?', '%' + content + '%')
     end
   end
-
+  
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_2day_ago, -> { where(created_at: 2.day.ago.all_day) }
+  scope :created_3day_ago, -> { where(created_at: 3.day.ago.all_day) }
+  scope :created_4day_ago, -> { where(created_at: 4.day.ago.all_day) }
+  scope :created_5day_ago, -> { where(created_at: 5.day.ago.all_day) }
+  scope :created_6day_ago, -> { where(created_at: 6.day.ago.all_day) }
+  
+  
+  
 end
